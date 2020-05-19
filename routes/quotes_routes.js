fb6c3db0545aca71808136d22fbba76cc4bc23b4
dms91 <drk7891@gmail.com>
2020-05-19 00:48:39 -0400

    add lookup by id and name

--- routes/quotes_routes.js
+++ routes/quotes_routes.js
@@ -8,7 +8,7 @@ const client = new Client({
     ssl: {
         rejectUnauthorized: false
     }
-});
+})
 
 client.connect()
 
@@ -16,15 +16,31 @@ routes.get('/', (req, res) => {
     res.status(200).json({message: "Connected!"})
 })
 
-routes.get('/all', (request, result) => {
+routes.get('/quotes', (request, result) => {
     client.query('SELECT * FROM quote', (err, res) => {
-        if (err) throw err;
+        if (err) throw err
         for (let row of res.rows) {
             // if need to do something with each row
         }
         result.status(200).json(res.rows)
-        client.end();
-      });
+        client.end()
+      })
+})
+
+routes.get('/quotes/id/:id', (request, result) => {
+    client.query('SELECT * FROM quote WHERE id=' + request.params.id, (err, res) => {
+        if (err) throw err
+        result.status(200).json(res.rows)
+    })
+})
+
+routes.get('/quotes/person/:person', (request, result) => {
+    var name = request.params.person[0].toUpperCase() + request.params.person.substring(1)
+    console.log(name)
+    client.query('SELECT * FROM quote WHERE person=' + name, (err, res) => {
+        if (err) throw err
+        result.status(200).json(res.rows)
+    })
 })
 
 module.exports = routes
